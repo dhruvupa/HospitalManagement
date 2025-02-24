8457afd053506433c047c3a0ca82b334969b572d
dhruvupa <68422575+dhruvupa@users.noreply.github.com>
2025-02-24 21:41:53 +0000

    Add files via upload

---
+++ HospitalManagementSystem/src/main/java/com/example/hms/HospiitalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospiitalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospiitalManagementSystemApplication.class, args);
+	}
+
+}
